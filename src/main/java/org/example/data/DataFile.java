eb958608a26dcaf83a389a02146d38ef43adbe3f
Trang1wf <daitrang20072005@gmail.com>
2024-06-23 22:30:59 +0700

    update

---
+++ src/main/java/org/example/data/DataFile.java
@@ -0,0 +1,25 @@
+package org.example.data;
+
+
+import java.io.Serializable;
+
+import org.example.tags.*;
+
+@SuppressWarnings("serial")
+public class DataFile implements Serializable{
+
+    @SuppressWarnings("unused")
+    private String openTags = Tags.FILE_DATA_OPEN_TAG;
+    @SuppressWarnings("unused")
+    private String closeTags = Tags.FILE_DATA_CLOSE_TAG;
+    public byte[] data;
+
+    public DataFile() {
+        data = new byte[Tags.MAX_MSG_SIZE];
+    }
+
+    public DataFile(int size) {
+        data = new byte[size];
+    }
+}
+
